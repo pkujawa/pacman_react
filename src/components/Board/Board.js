08839c4041cf62126e74baf3ad8265b21b68aacf
pkujawa <p.kujawa2@op.pl>
2020-09-28 00:48:18 +0200

    food on whole board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,11 +6,43 @@ import Food from '../Food';
 import './style.css';
 
 class Board extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.foods=[];
+        this.amountOfFood = (window.innerWidth) * (window.innerHeight - this.props.topScoreBoardHeight) / (this.props.foodSize * this.props.foodSize);
+        for (let i = 0; i < this.amountOfFood; i++) {
+            this['food' + i] = React.createRef();
+        }
+    }
     render() {
+        const {foodSize, border} = this.props;
+        let foods = [];
+        let currentTop = 0;
+        let currentLeft = 1 * foodSize;
+        for (let i = 0; i < this.amountOfFood; i++) {
+            if (currentLeft + foodSize >= (window.innerWidth - border)) {
+                currentTop += foodSize;
+                currentLeft = 0;
+            }
+            if (currentTop + foodSize >= (window.innerHeight - border - this.props.topScoreBoardHeight)) {
+                break;
+            }    
+
+            const position = {left: currentLeft, top: currentTop};
+            currentLeft += foodSize;
+            foods.push(
+                <Food
+                    key = {`food-elem-${i}`}
+                    position={position}
+                    ref={this['food' + i]}
+                />
+            )
+        }
         return (
             <div className="board">
-                <Food position={{top: 100, left: 100}} />
-                {/* <Food /> */}
+                {foods}
                 <Pacman />
                 <Ghost color="red" />
                 <Ghost color="pink"/>
@@ -21,4 +53,9 @@ class Board extends Component {
     }
 }
 
+Board.defaultProps = {
+    foodSize: 50,
+    border: 10 *2,
+    topScoreBoardHeight: 50
+}
 export default Board;
