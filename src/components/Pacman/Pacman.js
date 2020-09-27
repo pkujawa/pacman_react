d4d309ead10aa92ebbf3fbd5dbfa17a27b9ea67b
pkujawa <p.kujawa2@op.pl>
2020-09-27 16:39:43 +0200

    pacman svg added

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
+import { ReactComponent as PacmanSvg } from './pacman.svg';
 
+import './style.css';
 class Pacman extends Component {
+
+
+    state = {
+        direction: 'right', 
+        position: {
+            top: 100,
+            left: 50
+        }
+    }
     render() {
         return (
-            <div className="pacman">
-                Pacman
+            <div className="pacman"
+            style ={this.state.position}
+            >
+            <PacmanSvg />
             </div>
         );
     }
 
 }
 
+Pacman.defaultProps = {
+    step: 50, // 50px
+    size: 50, // pacman size: 50x50px
+    border: 10 * 2,
+    topScoreBoardHeight: 50, 
+}
 export default Pacman;
