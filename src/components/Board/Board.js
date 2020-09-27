1543d122932c34b1fccd45b92223c0204f106d0e
pkujawa <p.kujawa2@op.pl>
2020-09-28 01:21:48 +0200

    increasing score on eating

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -10,10 +10,56 @@ class Board extends Component {
     constructor(props) {
         super(props);
 
+        this.pacmanRef = React.createRef();
+
         this.foods=[];
         this.amountOfFood = (window.innerWidth) * (window.innerHeight - this.props.topScoreBoardHeight) / (this.props.foodSize * this.props.foodSize);
         for (let i = 0; i < this.amountOfFood; i++) {
             this['food' + i] = React.createRef();
+        }
+    }
+
+    componentDidMount() {
+        this.intervalFood = setInterval(this.lookForEat, 100);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalFood);
+    }
+
+    lookForEat = () => {
+        const pacmanX = this.pacmanRef.current.state.position.left;
+        const pacmanY = this.pacmanRef.current.state.position.top;
+        const pacmanSize = this.pacmanRef.current.props.size;
+
+        const pacmanLastX = pacmanX + pacmanSize/2;
+        const pacmanLastY = pacmanY + pacmanSize/2;
+
+        for(let i=0; i <= this.amountOfFood; i++) {
+            const currentFood = this['food' + i].current;
+            if (currentFood) {
+                const currentFoodX = currentFood.state.position.left;
+                const currentFoodY = currentFood.state.position.top;
+                const currentFoodSize = currentFood.props.foodSize;
+                const currentFoodLastX = currentFoodX + currentFoodSize/2;
+                const currentFoodLastY = currentFoodY + currentFoodSize/2;
+
+                if (
+                (pacmanX >= currentFoodX && pacmanX <= currentFoodLastX)
+                || (pacmanLastX >= currentFoodX && pacmanLastX <= currentFoodLastX)
+                ){
+                    if ((pacmanY >= currentFoodY && pacmanY <= currentFoodLastY)
+                    || (pacmanLastY >= currentFoodY && pacmanLastY <= currentFoodLastY)
+                    ) {
+                        if (!currentFood.state.hidden) {
+                            currentFood.ate(); 
+                            this.props.setScore((value) => value + 1);
+
+                        }
+                    }
+                }
+            }
+
         }
     }
     render() {
@@ -43,7 +89,7 @@ class Board extends Component {
         return (
             <div className="board">
                 {foods}
-                <Pacman />
+                <Pacman ref = {this.pacmanRef}/>
                 <Ghost color="red" />
                 <Ghost color="pink"/>
                 <Ghost color="blue"/>
