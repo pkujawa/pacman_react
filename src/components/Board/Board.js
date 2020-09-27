05b5cb376cad8546813296742f220f9d1a4c08d0
pkujawa <p.kujawa2@op.pl>
2020-09-27 15:01:56 +0200

    first commit

---
+++ src/components/Board/Board.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Board extends Component {
+    render() {
+        return (
+            <div className="board">
+                <div>Hello!</div>
+
+            </div>
+        )
+    }
+}
+
+export default Board;
