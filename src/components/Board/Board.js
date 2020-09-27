e7f11f63f1ab223b74c1d5d80a3b6286e7428353
pkujawa <p.kujawa2@op.pl>
2020-09-27 23:56:12 +0200

    food added

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import Pacman from '../Pacman';
 import Ghost from '../Ghost';
+import Food from '../Food';
 import './style.css';
 
 class Board extends Component {
     render() {
         return (
             <div className="board">
+                <Food position={{top: 100, left: 100}} />
                 {/* <Food /> */}
                 <Pacman />
                 <Ghost color="red" />
