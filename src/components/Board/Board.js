c93e14859abacfaf0f1d7c8988ea323f43fc5716
pkujawa <p.kujawa2@op.pl>
2020-09-27 16:03:30 +0200

    pacman class added

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
+import Pacman from '../Pacman';
 import './style.css';
 
 class Board extends Component {
     render() {
         return (
             <div className="board">
-                <div>Hello!</div>
-
+                {/* <Food /> */}
+                <Pacman />
+                {/* <Ghost /> */}
+                {/* <Ghost /> */}
             </div>
         )
     }
---
+++ src/components/Pacman/Pacman.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Pacman extends Component {
+    render() {
+        return (
+            <div className="pacman">
+                Pacman
+            </div>
+        );
+    }
+
+}
+
+export default Pacman;
