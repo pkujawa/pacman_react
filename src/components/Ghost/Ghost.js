012c4a58f32cdf87bc746520c865d197250cce5f
pkujawa <p.kujawa2@op.pl>
2020-09-27 21:53:13 +0200

    ghost added

---
+++ src/components/Ghost/Ghost.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+import './style.css';
+import {ReactComponent as GhostSvg } from './ghost.svg';
+class Ghost extends Component {
+
+    state = {
+        direction: 'left',
+        position: {
+            top: 50 * 3,
+            left: 50 * 3
+        }
+    }
+    render() {
+        const { color } = this.props
+        return (
+            <div style={this.state.position} className="ghost">
+                <GhostSvg className ={`ghost-${color}`} />
+            </div>
+        )
+    }
+}
+
+Ghost.defaultProps = {
+    color: 'red',
+    step: 50, // 50px
+    size: 50, // ghost size: 50x50px
+    border: 10 * 2,
+    topScoreBoardHeight: 50, 
+}
+export default Ghost;
